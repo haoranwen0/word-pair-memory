c8ad854b2d0e4266cba39b9126a5abe9377aade8
haoranwen0 <hranwen@mit.edu>
2022-12-23 22:02:59 -0500

    updated instructions

--- src/pages/Instructions.jsx
+++ src/pages/Instructions.jsx
@@ -8,12 +8,23 @@ const Instructions = () => {
   return (
     <div className="w-full h-screen flex justify-center p-16">
       <div className="w-1/2">
-        <p className="mb-4">
+        {/* <p className="mb-4">
           Welcome! In the follow game, you will see a sequence of pair of words.
           If you have seen the words in that exact sequence before, click the
           <span className="font-bold text-green-600"> Seen</span> button. If
           not, click the <span className="font-bold text-red-600">Nope</span>{" "}
           button. There are a total of 60 word pairs.{" "}
+          <span className="font-bold">ACCURACY IS MOST IMPORTANT</span>, but try
+          to react as fast as possible. <span className="font-bold">DON'T</span>{" "}
+          overthink it. Once you're ready, click Start in begin!
+        </p> */}
+        <p className="mb-4">
+          Welcome! In the follow game, you will see a sequence of pair of words.
+          If a sequence of words is repeated while{" "}
+          <span className="font-bold">IN THE GAME</span>, click the{" "}
+          <span className="font-bold text-green-600">Seen</span> button. If not,
+          click the <span className="font-bold text-red-600">Nope</span> button.
+          There are a total of 60 word pairs.{" "}
           <span className="font-bold">ACCURACY IS MOST IMPORTANT</span>, but try
           to react as fast as possible. <span className="font-bold">DON'T</span>{" "}
           overthink it. Once you're ready, click Start in begin!
